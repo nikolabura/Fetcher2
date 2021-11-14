dc6788f7f13c34488bb63dd1c0c8594aa23ce1a5
Nikola <no>
2021-11-14 00:00:08 -0500

    initial commit

---
+++ lib/fetcher2.ex
@@ -0,0 +1,18 @@
+defmodule Fetcher2 do
+  @moduledoc """
+  Documentation for `Fetcher2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fetcher2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fetcher2_test.exs
@@ -0,0 +1,8 @@
+defmodule Fetcher2Test do
+  use ExUnit.Case
+  doctest Fetcher2
+
+  test "greets the world" do
+    assert Fetcher2.hello() == :world
+  end
+end
